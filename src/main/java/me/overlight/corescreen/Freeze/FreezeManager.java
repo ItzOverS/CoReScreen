f9eb44d4af8b8a145471f2d2874699c822e63609
ItzOver <alireza@daneshian.ir>
2023-12-09 05:55:52 +0330

    Added eye blinding while frozen

--- src/main/java/me/overlight/corescreen/Freeze/FreezeManager.java
+++ src/main/java/me/overlight/corescreen/Freeze/FreezeManager.java
@@ -3,8 +3,11 @@ package me.overlight.corescreen.Freeze;
 import io.github.retrooper.packetevents.event.PacketListenerAbstract;
 import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
 import io.github.retrooper.packetevents.packettype.PacketType;
+import me.overlight.corescreen.CoReScreen;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -14,7 +17,7 @@ public class FreezeManager {
     private final static List<String> frozen = new ArrayList<String>();
     public final static List<String> freezeWhenLogin = new ArrayList<String>();
     public final static HashMap<String, Location> lastGround = new HashMap<>();
-
+    public final static boolean blindEye = CoReScreen.getInstance().getConfig().getBoolean("settings.freeze.blind-eye.enabled");
     public static boolean isFrozen(Player player) {
         return frozen.contains(player.getName());
     }
@@ -22,11 +25,13 @@ public class FreezeManager {
     public static void freezePlayer(Player player) {
         frozen.add(player.getName());
         player.setWalkSpeed(0);
+        if(blindEye) player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, Integer.MAX_VALUE, 1, false, false));
     }
 
     public static void unfreezePlayer(Player player) {
         frozen.remove(player.getName());
         player.setWalkSpeed(.2f);
+        if(blindEye) player.removePotionEffect(PotionEffectType.BLINDNESS);
     }
 
     public static void toggleFreeze(Player player) {
