92ad1e8ae013c57134e68c0449ca5e23f6adbc6a
ItzOver <alireza@daneshian.ir>
2023-12-09 16:49:15 +0330

    Fixed /freeze   command whitelist

--- src/main/java/me/overlight/corescreen/Freeze/PacketHandler.java
+++ src/main/java/me/overlight/corescreen/Freeze/PacketHandler.java
@@ -10,19 +10,21 @@ import io.github.retrooper.packetevents.packetwrappers.play.out.tabcomplete.Wrap
 import me.overlight.corescreen.CoReScreen;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class PacketHandler extends PacketListenerAbstract {
     private final boolean commandWhitelistEnabled = CoReScreen.getInstance().getConfig().getBoolean("settings.freeze.whitelist-commands.enabled"),
             commandWhitelistOverrideTabComplete = CoReScreen.getInstance().getConfig().getBoolean("settings.freeze.whitelist-commands.override-tabcomplete");
     private final List<String> whitelistCommands = CoReScreen.getInstance().getConfig().getStringList("settings.freeze.whitelist-commands.commands");
-    private String text = "";
+    private final HashMap<String, String> text = new HashMap<>();
 
     @Override
     public void onPacketPlayReceive(PacketPlayReceiveEvent e) {
-        if(commandWhitelistEnabled && commandWhitelistOverrideTabComplete && e.getPacketId() == PacketType.Play.Client.TAB_COMPLETE)
-            text = new WrappedPacketInTabComplete(e.getNMSPacket()).getText();
-        else if(commandWhitelistEnabled && e.getPacketId() == PacketType.Play.Client.CHAT){
+        if(e.getPlayer().hasPermission("corescreen.freeze.commandbypass")) return;
+        if(commandWhitelistEnabled && commandWhitelistOverrideTabComplete && e.getPacketId() == PacketType.Play.Client.TAB_COMPLETE && FreezeManager.isFrozen(e.getPlayer()))
+            text.put(e.getPlayer().getName(), new WrappedPacketInTabComplete(e.getNMSPacket()).getText());
+        else if(commandWhitelistEnabled && e.getPacketId() == PacketType.Play.Client.CHAT && FreezeManager.isFrozen(e.getPlayer())){
             WrappedPacketInChat packet = new WrappedPacketInChat(e.getNMSPacket());
             if(!packet.getMessage().startsWith("/")) return;
             if(!whitelistCommands.contains(packet.getMessage().substring(1).split(" ")[0])) e.setCancelled(true);
@@ -31,10 +33,11 @@ public class PacketHandler extends PacketListenerAbstract {
 
     @Override
     public void onPacketPlaySend(PacketPlaySendEvent e) {
-        if(commandWhitelistEnabled && commandWhitelistOverrideTabComplete && e.getPacketId() == PacketType.Play.Server.TAB_COMPLETE){
+        if(e.getPlayer().hasPermission("corescreen.freeze.commandbypass")) return;
+        if(commandWhitelistEnabled && commandWhitelistOverrideTabComplete && e.getPacketId() == PacketType.Play.Server.TAB_COMPLETE && FreezeManager.isFrozen(e.getPlayer())){
             WrappedPacketOutTabComplete packet = new WrappedPacketOutTabComplete(e.getNMSPacket());
-            if(text.split(" ").length > 1) return;
-            packet.setMatches(whitelistCommands.stream().filter(r -> r.startsWith(text)).toArray(String[]::new));
+            if(text.get(e.getPlayer().getName()).split(" ").length > 1) return;
+            packet.setMatches(whitelistCommands.stream().filter(r -> r.startsWith(text.get(e.getPlayer().getName()))).toArray(String[]::new));
         }
     }
 }
