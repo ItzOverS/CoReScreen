fc8faa5660e5b66a607c829a6fa794774d1749e4
ItzOver <alireza@daneshian.ir>
2023-12-09 17:10:22 +0330

    Asynced Profiling system

--- src/main/java/me/overlight/corescreen/Profiler/ProfileHandler.java
+++ src/main/java/me/overlight/corescreen/Profiler/ProfileHandler.java
@@ -17,37 +17,39 @@ public class ProfileHandler
 
     @Override
     public void run() {
-        HashMap<String, String> selections = new HashMap<>();
-        HashMap<String, List<Profiler.ProfileOption>> options = new HashMap<>();
-        for (Profiler profiler : ProfilerManager.profilers) {
-            selections.put(profiler.getStaff(), profiler.getWho());
-            options.put(profiler.getStaff(), profiler.options);
-        }
-        for (String k : selections.keySet()) {
-            List<String> m = new ArrayList<>();
-            Player who, staff;
-            try {
-                who = Bukkit.getOnlinePlayers().stream().filter(r -> r.getName().equals(selections.get(k))).collect(Collectors.toList()).get(0);
-                staff = Bukkit.getOnlinePlayers().stream().filter(r -> r.getName().equals(k)).collect(Collectors.toList()).get(0);
-            } catch (Exception ex) {
+        Bukkit.getScheduler().runTaskAsynchronously(CoReScreen.getInstance(), () -> {
+            HashMap<String, String> selections = new HashMap<>();
+            HashMap<String, List<Profiler.ProfileOption>> options = new HashMap<>();
+            for (Profiler profiler : ProfilerManager.profilers) {
+                selections.put(profiler.getStaff(), profiler.getWho());
+                options.put(profiler.getStaff(), profiler.options);
+            }
+            for (String k : selections.keySet()) {
+                List<String> m = new ArrayList<>();
+                Player who, staff;
+                try {
+                    who = Bukkit.getOnlinePlayers().stream().filter(r -> r.getName().equals(selections.get(k))).collect(Collectors.toList()).get(0);
+                    staff = Bukkit.getOnlinePlayers().stream().filter(r -> r.getName().equals(k)).collect(Collectors.toList()).get(0);
+                } catch (Exception ex) {
+                    for (Profiler.ProfileOption v : options.get(k)) {
+                        ProfilingSystem profiler = ProfilerManager.profilingSystems.stream().filter(r -> r.getMode().equals(v)).collect(Collectors.toList()).get(0);
+                        profiler.stopHandling(selections.get(k));
+                    }
+                    handlers.remove(selections.get(k));
+                    ProfilerManager.removeProfiler(k);
+                    continue;
+                }
                 for (Profiler.ProfileOption v : options.get(k)) {
                     ProfilingSystem profiler = ProfilerManager.profilingSystems.stream().filter(r -> r.getMode().equals(v)).collect(Collectors.toList()).get(0);
-                    profiler.stopHandling(selections.get(k));
+                    if (!handlers.contains(who.getName())) {
+                        handlers.add(who.getName());
+                        profiler.startHandling(who);
+                    }
+                    m.add(profiler.valueRequest(who, CoReScreen.translate("settings.profiler.formats." + profiler.getPlaceholder())));
                 }
-                handlers.remove(selections.get(k));
-                ProfilerManager.removeProfiler(k);
-                continue;
+                new PlayerActionBar((CoReScreen.getInstance().getConfig().getBoolean("settings.profiler.show-target") ? CoReScreen.translate("settings.profiler.formats.player") : "").replace("%player%", who.getName()) +
+                        String.join(CoReScreen.translate("settings.profiler.formats.splitter"), m)).send(staff);
             }
-            for (Profiler.ProfileOption v : options.get(k)) {
-                ProfilingSystem profiler = ProfilerManager.profilingSystems.stream().filter(r -> r.getMode().equals(v)).collect(Collectors.toList()).get(0);
-                if (!handlers.contains(who.getName())) {
-                    handlers.add(who.getName());
-                    profiler.startHandling(who);
-                }
-                m.add(profiler.valueRequest(who, CoReScreen.translate("settings.profiler.formats." + profiler.getPlaceholder())));
-            }
-            new PlayerActionBar((CoReScreen.getInstance().getConfig().getBoolean("settings.profiler.show-target") ? CoReScreen.translate("settings.profiler.formats.player") : "").replace("%player%", who.getName()) +
-                    String.join(CoReScreen.translate("settings.profiler.formats.splitter"), m)).send(staff);
-        }
+        });
     }
 }
